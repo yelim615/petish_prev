8e90fc31d69c60ab07d7bfd563741b45a6f23aa4
yelim <yelim615@naver.com>
2019-08-01 19:28:30 +0900

    [Update] merge시 수정사항 반영하여 commit

--- src/main/java/com/community/petish/mypage/mapper/MessageMapper.java
+++ src/main/java/com/community/petish/mypage/mapper/MessageMapper.java
@@ -12,18 +12,18 @@ public interface MessageMapper {
 	public int writeMessage(MessageRequestDTO dto);
 	
 	//2. 받은 쪽지 리스트 조회
-	//public ArrayList<MessageResponseDTO> getReceivedMessageList(Long user_id);
+	public ArrayList<MessageResponseDTO> getReceivedMessageList(Long user_id);
 	// 받은 쪽지 리스트 조회+페이징
-	public ArrayList<MessageResponseDTO> getReceivedMessageWithPaging(Criteria cri);
+	//public ArrayList<MessageResponseDTO> getReceivedMessageWithPaging(Criteria cri);
 	// 받은 쪽지 전체 갯수 가져오기
-	public int getReceivedCnt(Long user_id);
+	//public int getReceivedCnt(Long user_id);
 	
 	//3. 보낸 쪽지 리스트 조회
-	//public ArrayList<MessageResponseDTO> getSentMessageList(Long user_id);
+	public ArrayList<MessageResponseDTO> getSentMessageList(Long user_id);
 	// 보낸 쪽지 리스트 조회+페이징
-	public ArrayList<MessageResponseDTO> getSentMessageWithPaging(Criteria cri);
+	//public ArrayList<MessageResponseDTO> getSentMessageWithPaging(Criteria cri);
 	// 보낸 쪽지 전체 갯수 가져오기
-	public int getSentCnt(Long user_id);
+	//public int getSentCnt(Long user_id);
 	
 	//4. 받은 쪽지 상세조회
 	public MessageResponseDTO receivedMessageDetail(Long id);
